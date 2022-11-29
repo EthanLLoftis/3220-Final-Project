ca0334efd79f231febc6e59b122026d24f31d9ce
Ethan Loftis <elk3x@umsystem.edu>
2022-11-28 20:08:41 -0600

    fix: Linker Flags to define sdl library

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
+
 using namespace std;
 
 int main() {
 
+    if(SDL_Init(SDL_INIT_EVERYTHING) > 0) {
+        cout << "sdl init failed with error: " << SDL_GetError() << endl;
+        return EXIT_FAILURE;
+    }
+    
     cout << "SDL2 setup"<< endl;
 
     return EXIT_SUCCESS;
