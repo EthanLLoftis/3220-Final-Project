41b57a6276878bfafae7bb50d1608b96e77112de
Ethan Loftis <elk3x@umsystem.edu>
2022-12-01 10:20:24 -0600

    rename player object

--- src/CApp_OnInit.cpp
+++ src/CApp_OnInit.cpp
@@ -21,15 +21,15 @@ bool CApp::OnInit() {
 
     SDL_RenderClear(renderer);
 
-    SDL_Rect r;
-    r.x = 50;
-    r.y = 50;
-    r.w = 50;
-    r.h = 50;
+    SDL_Rect player;
+    player.x = 50;
+    player.y = 50;
+    player.w = 50;
+    player.h = 50;
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 
-    SDL_RenderFillRect(renderer, &r);
+    SDL_RenderFillRect(renderer, &player);
 
     SDL_RenderPresent(renderer);
 
